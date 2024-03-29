e16f9d02a242605e7702953b70321381afb5b8f9
Martin Schneider <martin@martingschneider.de>
2024-03-29 21:00:13 +0100

    Remove use of different .env files

--- webpack/plugins/index.ts
+++ webpack/plugins/index.ts
@@ -5,27 +5,14 @@ import HtmlWebpackPlugin from 'html-webpack-plugin';
 import NodemonPlugin from 'nodemon-webpack-plugin';
 import {ProgressPlugin} from 'webpack';
 
-import {deploymentEnv, webpackDir} from '../utils/env';
+import {webpackDir} from '../utils/env';
 
 
 export const nodemonPlugin = new NodemonPlugin();
 
 export const progressPlugin = new ProgressPlugin();
 
-let envFile;
-
-switch(deploymentEnv) {
-	case 'live':
-		envFile = './.env.production';
-		break;
-	case 'staging':
-		envFile = './.env.staging';
-		break;
-	default:
-		envFile = './.env';
-		break;
-}
-export const dotenvPlugin = new Dotenv({path: envFile});
+export const dotenvPlugin = new Dotenv();
 
 export const htmlWebpackPlugin = new HtmlWebpackPlugin({
 	filename: 'index.html',
